4228a05f85ed0362772b99563bde0ee9d168f136
Andrew Brinker <me@andrewbrinker.com>
2016-10-27 15:52:47 -0400

    Some Drow notes

--- drow/src/main.rs
+++ drow/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // Drow should:
+    //
+    // 1. Verify that you are currently in a drow project (identified by the
+    //    presence of a `Drow.toml` file in a parent directory).
+    // 2. Parse the configuration in the `Drow.toml` file and ensure that it
+    //    is a valid configuration, providing useful error messages otherwise.
+    // 3. Verify that the site directory is present.
+    //
+    // CLI API
+    //
+    // drow setup  - Create a new Drow project
+    // drow run    - Serve your Drow project locally
+    // drow build  - Build your Drow project once
+    // drow deploy - Deploy your Drow project
+    // drow post   - Create a new post and open your default editor
+    // drow page   - Create a new page and open your default editor
+    //
+    // Should probably draw inspiration from the internals of Cargo.
 }
