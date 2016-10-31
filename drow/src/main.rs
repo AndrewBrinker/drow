a71e3375aa92523cef6c3a12f498e2ecbb2f2965
Andrew Brinker <me@andrewbrinker.com>
2016-10-30 19:42:28 -0700

    Getting started

--- drow/src/main.rs
+++ drow/src/main.rs
@@ -1,3 +1,10 @@
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+
+static VERSION: &'static str = "1.0.0";
+static AUTHOR: &'static str = "Andrew Brinker <me@andrewbrinker.com>";
+
 fn main() {
     // Drow should:
     //
@@ -17,4 +24,46 @@ fn main() {
     // drow page   - Create a new page and open your default editor
     //
     // Should probably draw inspiration from the internals of Cargo.
+
+    let setup = SubCommand::with_name("setup")
+        .about("create a new drow site")
+        .author(AUTHOR)
+        .version(VERSION);
+
+    let run = SubCommand::with_name("run")
+        .about("serve your drow site locally")
+        .author(AUTHOR)
+        .version(VERSION);
+
+    let build = SubCommand::with_name("build")
+        .about("build your drow site once")
+        .author(AUTHOR)
+        .version(VERSION);
+
+    let deploy = SubCommand::with_name("deploy")
+        .about("deploy your drow project")
+        .author(AUTHOR)
+        .version(VERSION);
+
+    let post = SubCommand::with_name("post")
+        .about("create a new post with your default editor")
+        .author(AUTHOR)
+        .version(VERSION);
+
+    let page = SubCommand::with_name("page")
+        .about("create a new page with your default editor")
+        .author(AUTHOR)
+        .version(VERSION);
+
+    let matcher = App::new("drow")
+        .about("An opinionated static site builder")
+        .author(AUTHOR)
+        .version(VERSION)
+        .subcommand(setup)
+        .subcommand(run)
+        .subcommand(build)
+        .subcommand(deploy)
+        .subcommand(post)
+        .subcommand(page)
+        .get_matches();
 }
