9039a776dbb8d8c89973a8f19017b1f948bd5f4a
Andrew Brinker <me@andrewbrinker.com>
2016-10-31 21:14:11 -0700

    Working on it more

--- src/command/setup.rs
+++ src/command/setup.rs
@@ -1,5 +1,5 @@
 use std::path::Path;
-use std::fs::{metadata, read_dir};
+use std::fs::{File, metadata, read_dir};
 use git2::Repository;
 use slog_scope;
 
@@ -10,30 +10,43 @@ pub fn setup(directory: &str) {
     let path_buf = Path::new(directory).to_owned();
 
     if let Ok(path) = path_buf.canonicalize() {
-        if let Ok(meta) = metadata(&path) {
-            if meta.is_dir() {
-                if let Ok(read_dir) = read_dir(&path) {
-                    let contents: Vec<_> = read_dir.collect();
-
-                    if contents.len() == 0 {
-                        let repo = "https://github.com/AndrewBrinker/drow-template";
-
-                        if let Ok(..) = Repository::clone(repo, path) {
-                            info!(log, format!("drow site successfully setup at '{}'", directory));
-                        } else {
-                            error!(log, format!("failed to clone repository into '{}'", directory));
-                        }
+        let meta = match metadata(&path) {
+            Ok(meta) => meta,
+            Err(..) => {
+                if let Ok(file) = File::create(&path) {
+                    if let Ok(meta) = metadata(&path) {
+                        meta
                     } else {
-                        error!(log, format!("'{}' isn't empty", directory));
+                        error!(log, format!("couldn't read directory '{}'", directory));
+                        return;
                     }
                 } else {
-                    error!(log, format!("'{}' can't be edited with current permissions", directory));
+                    error!(log, format!("couldn't create directory '{}'", directory));
+                    return;
+                }
+            }
+        };
+
+        if meta.is_dir() {
+            if let Ok(read_dir) = read_dir(&path) {
+                let contents: Vec<_> = read_dir.collect();
+
+                if contents.len() == 0 {
+                    let repo = "https://github.com/AndrewBrinker/drow-template";
+
+                    if let Ok(..) = Repository::clone(repo, path) {
+                        info!(log, format!("drow site successfully setup at '{}'", directory));
+                    } else {
+                        error!(log, format!("failed to clone repository into '{}'", directory));
+                    }
+                } else {
+                    error!(log, format!("'{}' isn't empty", directory));
                 }
             } else {
-                error!(log, format!("'{}' isn't a directory", directory));
+                error!(log, format!("'{}' can't be edited with current permissions", directory));
             }
         } else {
-            error!(log, format!("'{}' can't be read", directory));
+            error!(log, format!("'{}' isn't a directory", directory));
         }
     } else {
         error!(log, format!("'{}' can't be canonicalized", directory));
