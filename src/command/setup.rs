2c079ae6b477a657e4e171aadaa5c22ee87cdcf8
Andrew Brinker <me@andrewbrinker.com>
2016-11-01 18:04:08 -0700

    Got setup basically working

--- src/command/setup.rs
+++ src/command/setup.rs
@@ -1,55 +1,56 @@
 use std::path::Path;
-use std::fs::{File, metadata, read_dir};
+use std::fs::{File, metadata, read_dir, create_dir};
 use git2::Repository;
 use slog_scope;
 
+// There are three cases:
+//
+// - The directory exists and isn't empty => error
+// - The directory exists and is empty => use it
+// - The directory doesn't exist => create it and use it
 pub fn setup(directory: &str) {
     let log = slog_scope::logger();
+    let directory = Path::new(directory);
+
+    // Sweet, sweet logging.
     info!(log, "Setting up drow site");
 
-    let path_buf = Path::new(directory).to_owned();
+    // Make sure the directory exists and create it if it doesn't.
+    if !directory.exists() {
+        warn!(log, format!("'{}' doesn't exist", directory.display()));
+        info!(log, format!("attempting to create directory '{}'", directory.display()));
 
-    if let Ok(path) = path_buf.canonicalize() {
-        let meta = match metadata(&path) {
-            Ok(meta) => meta,
+        match create_dir(directory) {
+            Ok(..) => info!(log, format!("created directory '{}'", directory.display())),
             Err(..) => {
-                if let Ok(file) = File::create(&path) {
-                    if let Ok(meta) = metadata(&path) {
-                        meta
-                    } else {
-                        error!(log, format!("couldn't read directory '{}'", directory));
-                        return;
-                    }
-                } else {
-                    error!(log, format!("couldn't create directory '{}'", directory));
-                    return;
-                }
+                error!(log, format!("couldn't create directory '{}'", directory.display()));
+                return;
             }
-        };
-
-        if meta.is_dir() {
-            if let Ok(read_dir) = read_dir(&path) {
-                let contents: Vec<_> = read_dir.collect();
-
-                if contents.len() == 0 {
-                    let repo = "https://github.com/AndrewBrinker/drow-template";
-
-                    if let Ok(..) = Repository::clone(repo, path) {
-                        info!(log, format!("drow site successfully setup at '{}'", directory));
-                    } else {
-                        error!(log, format!("failed to clone repository into '{}'", directory));
-                    }
-                } else {
-                    error!(log, format!("'{}' isn't empty", directory));
-                }
-            } else {
-                error!(log, format!("'{}' can't be edited with current permissions", directory));
-            }
-        } else {
-            error!(log, format!("'{}' isn't a directory", directory));
         }
+    }
+
+    // Get the contents of the directory as a vector.
+    let contents: Vec<_> = match read_dir(directory) {
+        Ok(directory_iter) => directory_iter.collect(),
+        Err(..) => {
+            error!(log, format!("couldn't read directory '{}'", directory.display()));
+            return;
+        }
+    };
+
+    // If that vector isn't empty, you've got problems!
+    if contents.len() != 0 {
+        error!(log, format!("directory '{}' isn't empty", directory.display()));
+        return;
+    }
+
+    // Otherwise, clone the repo.
+    let repo = "https://github.com/AndrewBrinker/drow-template";
+
+    if let Ok(..) = Repository::clone(repo, directory) {
+        info!(log, format!("drow site successfully setup at '{}'", directory.display()));
     } else {
-        error!(log, format!("'{}' can't be canonicalized", directory));
+        error!(log, format!("failed to clone repository into '{}'", directory.display()));
     }
 }
 
