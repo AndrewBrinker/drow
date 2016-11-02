5b7a9ac4613e7b0c68a12537cc14bb431f557590
Andrew Brinker <me@andrewbrinker.com>
2016-11-01 18:18:16 -0700

    Further improvements to setup command

--- src/command/setup.rs
+++ src/command/setup.rs
@@ -1,5 +1,5 @@
 use std::path::Path;
-use std::fs::{File, metadata, read_dir, create_dir};
+use std::fs;
 use git2::Repository;
 use slog_scope;
 
@@ -9,48 +9,54 @@ use slog_scope;
 // - The directory exists and is empty => use it
 // - The directory doesn't exist => create it and use it
 pub fn setup(directory: &str) {
+    let repo = "https://github.com/AndrewBrinker/drow-template";
     let log = slog_scope::logger();
     let directory = Path::new(directory);
+    let display = directory.display();
 
     // Sweet, sweet logging.
     info!(log, "Setting up drow site");
 
     // Make sure the directory exists and create it if it doesn't.
     if !directory.exists() {
-        warn!(log, format!("'{}' doesn't exist", directory.display()));
-        info!(log, format!("attempting to create directory '{}'", directory.display()));
+        warn!(log, format!("'{}' doesn't exist", display));
+        info!(log, format!("creating directory '{}'", display));
 
-        match create_dir(directory) {
-            Ok(..) => info!(log, format!("created directory '{}'", directory.display())),
+        match fs::create_dir(directory) {
+            Ok(..) => info!(log, format!("created directory '{}'", display)),
             Err(..) => {
-                error!(log, format!("couldn't create directory '{}'", directory.display()));
+                error!(log, format!("couldn't create directory '{}'", display));
                 return;
             }
         }
     }
 
+    // Make sure we're looking at a directory.
+    if !directory.is_dir() {
+        error!(log, format!("'{}' isn't a directory", display));
+        return;
+    }
+
     // Get the contents of the directory as a vector.
-    let contents: Vec<_> = match read_dir(directory) {
-        Ok(directory_iter) => directory_iter.collect(),
+    let contents: Vec<_> = match directory.read_dir() {
+        Ok(directory_iter) => directory_iter.filter(|r| r.is_ok()).collect(),
         Err(..) => {
-            error!(log, format!("couldn't read directory '{}'", directory.display()));
+            error!(log, format!("couldn't read directory '{}'", display));
             return;
         }
     };
 
     // If that vector isn't empty, you've got problems!
-    if contents.len() != 0 {
-        error!(log, format!("directory '{}' isn't empty", directory.display()));
+    if !contents.is_empty() {
+        error!(log, format!("directory '{}' isn't empty", display));
         return;
     }
 
     // Otherwise, clone the repo.
-    let repo = "https://github.com/AndrewBrinker/drow-template";
-
     if let Ok(..) = Repository::clone(repo, directory) {
-        info!(log, format!("drow site successfully setup at '{}'", directory.display()));
+        info!(log, format!("drow site successfully setup at '{}'", display));
     } else {
-        error!(log, format!("failed to clone repository into '{}'", directory.display()));
+        error!(log, format!("failed to clone repository into '{}'", display));
     }
 }
 
