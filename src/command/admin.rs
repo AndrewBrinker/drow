b0de4c0bf27a2ff53bc92d26e43c912be2c41fea
Andrew Brinker <me@andrewbrinker.com>
2017-12-31 08:03:12 -0800

    Cleaned out admin.rs

--- src/command/admin.rs
+++ src/command/admin.rs
@@ -4,15 +4,5 @@ use config::Config;
 /// fully-built Drow site, the other to serve an admin panel to manage site
 /// configuration, edit pages, add pages, edit posts, and add posts.
 pub fn admin(config: Config) {
-    println!("checking that we're in a drow repo");
-
-    let config_file = config.config_file();
-    if !config_file.exists() {
-        println!("we are not in a drow repo");
-        println!("cannot continue. Exiting...");
-        return;
-    }
-
-    // TODO: implement the rest of it
     unimplemented!();
 }
