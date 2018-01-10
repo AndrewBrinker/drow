ef22f2fefa1548c5a4e1f678c61e28d604063e29
Andrew Brinker <me@andrewbrinker.com>
2018-01-09 18:23:00 -0800

    Added a comment with a sketch of how building should work

--- src/workers/builder.rs
+++ src/workers/builder.rs
@@ -21,6 +21,65 @@ impl Builder {
         //
         // Pages and Posts may use Templates.
 
+        // Probably want to start by copying over the assets.
+        //
+        // Then go through the posts and build up a data structure representing them.
+        //
+        // Make sure also to load up Drow.toml
+        //
+        // Load the posts and the Drow.toml into a uniform data structure which the page templates
+        // can use.
+        //
+        // Then do the pages.
+        //
+        // For the templating, I don't think anything to complicated is needed.
+        //
+        // Any post or page can put something like the following at the top:
+        //
+        //     template: hello.html
+        //     ---
+        //
+        //     <p>Hello!</p>
+        //
+        // And the template choice may look like this:
+        //
+        //     template: main.html
+        //     ---
+        //
+        //     <div class="hello">
+        //         {{! content }}
+        //     </div>
+        //
+        // Which would then get passed along to main.html, which may look like this:
+        //
+        //     <!doctype html>
+        //     <html lang="en-US">
+        //         <head>
+        //             <meta charset="utf-8">
+        //             <title>{{= data.title }}</title>
+        //         </head>
+        //         <body>
+        //             {{! content }}
+        //         </body>
+        //     </html>
+        //
+        // So the end result would look like this, assuming Drow.toml had title set to "Hello":
+        //
+        //     <!doctype html>
+        //     <html lang="en-US">
+        //         <head>
+        //             <meta charset="utf-8">
+        //             <title>Hello</title>
+        //         </head>
+        //         <body>
+        //             <div class="hello">
+        //                 <p>Hello!</p>
+        //             </div>
+        //         </body>
+        //     </html>
+        //
+        // So the end result would look like this, assuming Drow.toml had title set to "Hello":
+
         Ok(())
     }
 }
